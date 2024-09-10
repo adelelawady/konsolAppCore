cbdb1fbb0524492dcdd98c80aad9dff410a84fb1
adel El-awady <adel50ali50@gmail.com>
2024-09-10 10:04:41 +0300

    Revert "last"
    This reverts commit ad9f6f7cf52cea92be29fc62fb3a9572d7c67e16.

--- src/main/java/com/konsol/core/config/SecurityConfiguration.java
+++ src/main/java/com/konsol/core/config/SecurityConfiguration.java
@@ -83,6 +83,7 @@ public class SecurityConfiguration {
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            //.antMatchers("/api/invoices/**").hasAnyRole("INVOICE_READ", "INVOICE_CREATE", "INVOICE_UPDATE", "INVOICE_DELETE") // Only users with appropriate roles can access invoices
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/health/**").permitAll()
