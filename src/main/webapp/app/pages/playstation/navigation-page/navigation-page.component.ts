f3b18d89ff06bc1dd8027c874a061564985afd90
adel ali mohamed <adel50ali50@gmail.com>
2024-12-15 13:25:24 +0200

    remove demo items from navigation page

--- src/main/webapp/app/pages/playstation/navigation-page/navigation-page.component.ts
+++ src/main/webapp/app/pages/playstation/navigation-page/navigation-page.component.ts
@@ -6,18 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-page.component.scss'],
 })
 export class NavigationPageComponent {
-  playstationGames = [
-    { title: 'God of War Ragnarök', image: 'god-of-war-ragnarok.jpg' },
-    { title: 'Horizon Forbidden West', image: 'horizon-forbidden-west.jpg' },
-    { title: 'Spider-Man: Miles Morales', image: 'spider-man-miles-morales.jpg' },
-    { title: 'Ghost of Tsushima', image: 'ghost-of-tsushima.jpg' },
-    { title: 'The Last of Us Part II', image: 'the-last-of-us-part-ii.jpg' },
-    { title: 'Ratchet & Clank: Rift Apart', image: 'ratchet-clank-rift-apart.jpg' },
-    { title: 'Returnal', image: 'returnal.jpg' },
-    { title: "Demon's Souls", image: 'demons-souls.jpg' },
-    { title: 'Sackboy: A Big Adventure', image: 'sackboy-a-big-adventure.jpg' },
-    { title: "Marvel's Spider-Man: Remastered", image: 'marvels-spider-man-remastered.jpg' },
-  ];
 
   constructor() {
     // Empty constructor
