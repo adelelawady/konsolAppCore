b00b38cd0206843cd08e221776380dee68dc6256
adel elawady <adel50ali50@gmail.com>
2024-12-17 00:22:45 +0200

    nav page ps

--- src/main/webapp/app/pages/playstation/playstation-routing.module.ts
+++ src/main/webapp/app/pages/playstation/playstation-routing.module.ts
@@ -1,38 +1,103 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { inject, NgModule } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterModule, Routes } from '@angular/router';
 import { NavigationPageComponent } from './navigation-page/navigation-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DevicesControlComponent } from './devices-control/devices-control.component';
 import { DeviceTypeControlComponent } from './device-type-control/device-type-control.component';
 import { ProductsControlComponent } from './products-control/products-control.component';
 import { SessionListComponent } from './components/session/session-list.component';
+import { PlaystationLayoutComponent } from 'app/layouts/playstation/playstation-layout.component';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
+import { PlaystationDeviceUpdateComponent } from 'app/entities/playstation-device/update/playstation-device-update.component';
+import playstationDeviceResolve from 'app/entities/playstation-device/route/playstation-device-routing-resolve.service';
+import { DeviceTypeFormComponent } from './device-type-control/device-type-form.component';
+import { PlaystationResourceService } from 'app/core/konsolApi/api/playstationResource.service';
+import { of } from 'rxjs';
+
 
 const routes: Routes = [
   {
     path: '',
-    component: NavigationPageComponent
+    component: PlaystationLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: NavigationPageComponent,
+        canActivate: [UserRouteAccessService],
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [UserRouteAccessService],
+      },
+      {
+        path: 'controls',
+        children: [
+          {
+            path: '',
+            component: DevicesControlComponent,
+          },
+          {
+            path: 'new',
+            component: PlaystationDeviceUpdateComponent,
+            resolve: {
+              playstationDevice: playstationDeviceResolve,
+            },
+          },
+          {
+            path: ':id/edit',
+            component: PlaystationDeviceUpdateComponent,
+            resolve: {
+              playstationDevice: playstationDeviceResolve,
+            },
+          },
+        ],
+      },
+      {
+        path: 'device-types',
+        children: [
+          {
+            path: '',
+            component: DeviceTypeControlComponent,
+          },
+          {
+            path: 'new',
+            component: DeviceTypeFormComponent,
+          },
+          {
+            path: ':id/edit',
+            component: DeviceTypeFormComponent,
+            resolve: {
+              deviceType: (route: ActivatedRouteSnapshot) => {
+                const id = route.paramMap.get('id');
+                if (id) {
+                  return inject(PlaystationResourceService).getDeviceType(id);
+                }
+                return of(null);
+              },
+            },
+          },
+        ],
+      },
+      {
+        path: 'products',
+        component: ProductsControlComponent,
+        data: { pageTitle: 'Products Control' }
+      },
+
+      {
+        path: 'sessions',
+        component: SessionListComponent,
+        data: { pageTitle: 'Sessions History' }
+      },
+    ],
   },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'devices',
-    component: DevicesControlComponent
-  },
-  {
-    path: 'device-types',
-    component: DeviceTypeControlComponent
-  },
-  {
-    path: 'products',
-    component: ProductsControlComponent
-  },
-  {
-    path: 'sessions',
-    component: SessionListComponent
-  }
 ];
+
+
+
+
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
