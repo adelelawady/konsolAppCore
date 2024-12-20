84e7e66e91b7aeda53cd186a2cd6036e218e6e1a
Adel Elawady <adel50ali50@gmail.com>
2024-12-20 14:55:17 +0200

    fix get device by category

--- src/main/java/com/konsol/core/repository/PlaystationDeviceRepository.java
+++ src/main/java/com/konsol/core/repository/PlaystationDeviceRepository.java
@@ -1,6 +1,7 @@
 package com.konsol.core.repository;
 
 import com.konsol.core.domain.playstation.PlaystationDevice;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PlaystationDeviceRepository extends MongoRepository<PlaystationDevice, String> {
-    Optional<PlaystationDevice> findAllByCategory(String category);
+    List<PlaystationDevice> findAllByCategory(String category);
 }
