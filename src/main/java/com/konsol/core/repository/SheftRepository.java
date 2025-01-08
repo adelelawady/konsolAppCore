a38f11d987f6daf6a9984295896384a4c12128b8
Adel Elawady <adel50ali50@gmail.com>
2025-01-08 11:42:02 +0200

    feat: add pagination and sorting capabilities to SheftRepository ✨ 📄
    This commit introduces new methods in `SheftRepository.java` to support paginated and sorted queries for `Sheft` entities. The added methods include:
    - `findAllByOrderByEndTimeDesc(Pageable pageable)`: Retrieves `Sheft` entities sorted by `endTime` in descending order with pagination.
    - `findAllByOrderByStartTimeDesc(Pageable pageable)`: Retrieves `Sheft` entities sorted by `startTime` in descending order with pagination.
    
    These enhancements improve data retrieval flexibility and efficiency, enabling better handling of large datasets. ✨ 📄

--- src/main/java/com/konsol/core/repository/SheftRepository.java
+++ src/main/java/com/konsol/core/repository/SheftRepository.java
@@ -1,7 +1,12 @@
 package com.konsol.core.repository;
 
 import com.konsol.core.domain.Sheft;
+
+import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +16,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SheftRepository extends MongoRepository<Sheft, String> {
     Optional<Sheft> findByActiveTrue();
+
+
+
+    Page<Sheft> findAllByOrderByEndTimeDesc(Pageable pageable);
+
+    Page<Sheft> findAllByOrderByStartTimeDesc(Pageable pageable);
+
+
+
 }
